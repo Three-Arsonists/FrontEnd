d27dc139d649e8e13283d2d3062ba432f0585761
insa14 <kong3361@naver.com>
2022-03-15 23:15:21 +0900

    수정

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,8 +3,8 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app){
     app.use(
-        createProxyMiddleware("/users/signup",{
-            target:"http://localhost:8000/",
+        createProxyMiddleware("/users",{
+            target:"http://localhost:5000/",
             changeOrigin:true,
         })
     )
