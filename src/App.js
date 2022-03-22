8735124ccc9ff9983ceef54989eb6ba20be077b9
insa14 <kong3361@naver.com>
2022-03-22 23:46:08 +0900

    bug

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
-import './App.css';
-import { Route, Routes } from 'react-router-dom';
-import SignIn from './pages/SignIn';
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import SignIn from "./pages/SignIn";
+import Home from "./pages/Home";
+
 // import { useCookies } from "react-cookie";
 // import { useEffect } from 'react';
 
 function App() {
   // const [cookies, setCookie, removeCookie] = useCookies(['access_token']);
-  
+
   // useEffect(() => {
   //   setCookie('access_token','')
   // },[])
-  
+
   return (
     <Routes>
-      <Route path="/" element={<SignIn/>} />
-      <Route path="/signin" element={<SignIn/>} />
+      <Route path="/" element={<Home />} />
+      <Route path="/signin" element={<SignIn />} />
     </Routes>
   );
 }
